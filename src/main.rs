f82965c2c0f44b97ec8b9c76980b4023b8c07988
shaicoin <shaicoin@proton.me>
2024-10-14 00:12:21 -0400

    Handle reconnection gracefully

--- src/main.rs
+++ src/main.rs
@@ -28,11 +28,11 @@ mod api;
 use utils::*;
 use models::*;
 use hasher::*;
+use rand::Rng;
 use colored::*;
 use std::thread;
 use ascii_art::*;
 use std::sync::Arc;
-use std::process::exit;
 use std::time::Duration;
 use tokio::sync::{Mutex};
 use crate::api::MinerState;
@@ -71,12 +71,7 @@ async fn main() {
 
     let bailout_timer = args.vdftime_parsed;
     let miner_id = args.address.unwrap();
-    let request = args.pool.unwrap().clone().into_client_request().unwrap();
-    let (ws_stream, _) = connect_async(request)
-        .await
-        .expect("Failed to connect to WebSocket server");
 
-    let (write, mut read) = ws_stream.split();
     let (server_sender, server_receiver) = mpsc::channel::<String>();
 
     let current_job: Arc<Mutex<Option<Job>>> = Arc::new(Mutex::new(None));
@@ -87,14 +82,6 @@ async fn main() {
         rejected_shares: Arc::new(AtomicUsize::new(0)),
         hashrate_samples: Arc::new(Mutex::new(Vec::new())),
         version: String::from("1.0.0"),
-    });
-
-    // Spawn write task to send solutions to the server
-    tokio::spawn(async move {
-        let mut write = write;
-        while let Ok(msg) = server_receiver.recv() {
-            write.send(Message::Text(msg)).await.unwrap();
-        }
     });
 
     // Spawn worker threads for processing jobs
@@ -184,71 +171,109 @@ async fn main() {
         }
     });
 
-
     let api_state = miner_state.clone();
     tokio::spawn(api::start_http_server(api_state));
 
-    // Run the read task
     let current_job_clone = Arc::clone(&current_job);
-    
+    let request_clone = args.pool.unwrap().clone();
+
+    let server_receiver = Arc::new(Mutex::new(server_receiver));
+
     loop {
-        match read.next().await {
-            Some(Ok(msg)) => {
-                let server_message: ServerMessage =
-                    serde_json::from_str(msg.to_text().unwrap()).unwrap();
-                match server_message.r#type.as_str() {
-                    "job" => {
-                        if let (Some(job_id), Some(data), Some(target)) = (
-                            server_message.job_id.clone(),
-                            server_message.data.clone(),
-                            server_message.target.clone(),
-                        ) {
-                            let new_job = Job {
-                                job_id: job_id.clone(),
-                                data: data.clone(),
-                                target: target.clone(),
-                            };
+        let request = request_clone.clone().into_client_request().unwrap();
+        let (ws_stream, _) = match connect_async(request).await {
+            Ok((ws_stream, response)) => {
+                (ws_stream, response)
+            }
+            Err(_e) => {
+                let delay_secs = rand::thread_rng().gen_range(5..30);
+                println!("{}", format!("Failed to connect will retry in {} seconds...", delay_secs).red());
+                tokio::time::sleep(Duration::from_secs(delay_secs)).await;
+                continue;
+            }
+        };
 
-                            let mut job_guard = current_job_clone.lock().await;
-                            *job_guard = Some(new_job);
+        let (write, mut read) = ws_stream.split();
 
-                            println!(
-                                "{} {}",
-                                "Received new job:".bold().blue(),
-                                format!(
-                                    "ID = {}, Data = {}, Target = {}",
-                                    job_id, data, target
-                                )
-                                .bold()
-                                .yellow()
-                            );
+        // Spawn write task to send solutions to the server
+        let server_receiver_clone = Arc::clone(&server_receiver);
+        tokio::spawn(async move {
+            let mut write = write;
+            while let Ok(msg) = {
+                let receiver = server_receiver_clone.lock().await;
+                receiver.recv()
+            } {
+                write.send(Message::Text(msg)).await.unwrap();
+            }
+        });
+        
+        loop {
+            match read.next().await {
+                Some(Ok(msg)) => {
+                    let server_message: ServerMessage =
+                        serde_json::from_str(msg.to_text().unwrap()).unwrap();
+                    match server_message.r#type.as_str() {
+                        "job" => {
+                            if let (Some(job_id), Some(data), Some(target)) = (
+                                server_message.job_id.clone(),
+                                server_message.data.clone(),
+                                server_message.target.clone(),
+                            ) {
+                                let new_job = Job {
+                                    job_id: job_id.clone(),
+                                    data: data.clone(),
+                                    target: target.clone(),
+                                };
+    
+                                let mut job_guard = current_job_clone.lock().await;
+                                *job_guard = Some(new_job);
+    
+                                println!(
+                                    "{} {}",
+                                    "Received new job:".bold().blue(),
+                                    format!(
+                                        "ID = {}, Data = {}, Target = {}",
+                                        job_id, data, target
+                                    )
+                                    .bold()
+                                    .yellow()
+                                );
+                            }
                         }
+                        "accepted" => {
+                            miner_state.accepted_shares.fetch_add(1, Ordering::Relaxed);
+                            println!(
+                                "{}",
+                                format!("Share accepted")
+                                    .bold()
+                                    .green()
+                            );
+                            display_share_accepted();
+                        }
+                        "rejected" => {
+                            miner_state.rejected_shares.fetch_add(1, Ordering::Relaxed);
+                            println!("{}", "Share rejected.".red());
+                        }
+                        _ => {}
                     }
-                    "accepted" => {
-                        miner_state.accepted_shares.fetch_add(1, Ordering::Relaxed);
-                        println!(
-                            "{}",
-                            format!("Share accepted")
-                                .bold()
-                                .green()
-                        );
-                        display_share_accepted();
-                    }
-                    "rejected" => {
-                        miner_state.rejected_shares.fetch_add(1, Ordering::Relaxed);
-                        println!("{}", "Share rejected.".red());
-                    }
-                    _ => {}
+                }
+                Some(Err(_e)) => {
+                    println!("{}", "WebSocket connection closed. Will sleep then try to reconnect".red());
+                    break;
+                }
+                None => {
+                    println!("{}", "WebSocket connection closed. Will sleep then try to reconnect.".red());
+                    break;
                 }
             }
-            Some(Err(e)) => {
-                println!("Error receiving message: {:?}", e);
-                exit(0);
-            }
-            None => {
-                println!("WebSocket connection closed.");
-                exit(0);
-            }
         }
+
+        let mut job_guard = current_job_clone.lock().await;
+        *job_guard = None;
+
+        let delay_secs = rand::thread_rng().gen_range(11..42);
+        println!("{}", format!("Reconnecting in {} seconds...", delay_secs).yellow());
+        tokio::time::sleep(Duration::from_secs(delay_secs)).await;
+        println!("{}", "Attempting to reconnect...".red());
     }
 }
